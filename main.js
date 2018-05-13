e92ef93d19c532ae94d30eeab840c9bcfa42487b
jameskirsch <jjamesdk@yahoo.com>
2018-05-13 18:20:06 -0400

    added pie chart for gender

--- main.js
+++ main.js
@@ -4,50 +4,67 @@ $(document).ready(function () {
     // Using an API that generates Random Users to populate Users Table and make Bar Chart
     // Based on registered users by date range
     $.ajax({
-        url: 'https://randomuser.me/api/?results=50',
+        url: 'https://randomuser.me/api/?results=50&noinfo',
         method: 'GET',
         success: function(data) {
-
+            console.log(data);
             var date = new Date();
-            var registeredBeforeOrIn2010 = 0;
-            var registeredAfter2010 = 0;
-            var registeredIn2013 = 0;
+            var registeredBeforeOrIn2008 = 0;
+            var registeredAfter2008 = 0;
+            var registeredIn2010 = 0;
+            var genderIsMale = 0;
+            var genderIsFemale = 0;
 
             data.results.forEach(user => {
                 var registered = new Date(user.registered);
                 var yearRegistered = registered.getFullYear();
-
                 user.registered = yearRegistered;
 
-                if (yearRegistered === 2013) {
-                    registeredIn2013 += 1;
-                } else if (yearRegistered <= 2010) {
-                    registeredBeforeOrIn2010 += 1;
-                } else if (yearRegistered > 2010) {
-                    registeredAfter2010 += 1;
+                if (yearRegistered === 2010) {
+                    registeredIn2010 += 1;
+                } else if (yearRegistered <= 2008) {
+                    registeredBeforeOrIn2008 += 1;
+                } else if (yearRegistered > 2008) {
+                    registeredAfter2008 += 1;
+                }
+
+                if (user.gender === 'male') {
+                    genderIsMale += 1;
+                } else {
+                    genderIsFemale += 1;
                 }
             });
 
             // The ordering here matters to match labels on bar chart
             var registrationCounts = [];
             registrationCounts.push(
-                registeredBeforeOrIn2010,
-                registeredAfter2010,
-                registeredIn2013
+                registeredBeforeOrIn2008,
+                (registeredAfter2008),
+                registeredIn2010
             );
             var chartLabels = [
-                'Before or In 2010',
-                'After 2010',
-                'In 2013'
+                'Before or In 2008',
+                'After 2008',
+                'In 2010'
             ];
 
-            CreateChart(
+            CreateBarChart(
                 'bar-chart',
                 'Registered Users',
                 chartLabels,
                 registrationCounts,
                 'Registered Users by Date Range',
                 'bar'
+            );
+
+            var pieChartData = [genderIsMale, genderIsFemale];
+            var pieChartLabels = ['Male', 'Female'];
+
+            CreatePieChart(
+                'pie-chart',
+                pieChartData,
+                pieChartLabels,
+                'Users by Gender'
             );
 
             // Saying 'data' is required for this
@@ -70,16 +87,40 @@ $(document).ready(function () {
         }
     });
 
-    function CreateChart(
-        elementId, 
+    function CreatePieChart(
+        elementId,
+        dataValues,
+        dataLabels,
+        title
+    ) {
+        var myPieChart = new Chart($('#' + elementId),{
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: dataValues,
+                    
+                    backgroundColor: ["#4F84C4", "#F6D155"]
+                }],
+                labels: dataLabels
+            },
+            options: {
+                title: {
+                    text: title,
+                    display: true,
+                }
+            }
+        });
+    }
+
+    function CreateBarChart(
+        elementId,
         xLabelTitle,
         xLabelSet,
         yValueSet,
-        chartTitle,
-        type
+        chartTitle
     ) {
         new Chart($('#' + elementId), {
-            type: type,
+            type: 'bar',
             data: {
                 labels: xLabelSet,
                 datasets: [{
